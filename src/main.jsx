d38ed6a267ee0bab42f63333b4e81f0b7c9bdb71
dmitrievolegg <dmitriev.olegg@gmail.com>
2024-12-09 23:16:00 +0100

    Add hw 01

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import "modern-normalize";
+import App from "./components/App";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
